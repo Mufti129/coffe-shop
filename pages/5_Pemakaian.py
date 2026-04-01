ce7dc1326594fa5c917dc433c6c51fd83a4ccf91
Mufti129 <ukhtytrader01@gmail.com>
2026-04-01 07:23:25 +0700

    5_Pemakaian.py

---
+++ pages/5_Pemakaian.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from services.sheets_service import connect, get_data
+from services.stock_service import calculate_usage
+from utils.filter import date_filter
+import config
+
+client = connect()
+
+sales = get_data(client, config.SPREADSHEET_NAME, config.SHEET_SALES)
+recipe = get_data(client, config.SPREADSHEET_NAME, config.SHEET_RECIPE)
+
+sales = date_filter(sales)
+
+usage = calculate_usage(sales, recipe)
+summary = usage.groupby('produk')['qty'].sum().reset_index()
+
+st.title("📉 Pemakaian Bahan")
+st.dataframe(summary)
