de6ab2fabd7e49512ea45034c57c6afc2b55b7ed
Mufti129 <ukhtytrader01@gmail.com>
2026-04-01 07:23:47 +0700

    4_Histori.py

---
+++ pages/4_Histori.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from services.sheets_service import connect, get_data
+from utils.filter import date_filter
+import config
+
+client = connect()
+
+df = get_data(client, config.SPREADSHEET_NAME, config.SHEET_TRANSACTIONS)
+
+user_filter = st.selectbox(
+    "Filter User",
+    ["Semua"] + list(df['user'].unique())
+)
+
+df = date_filter(df)
+
+if user_filter != "Semua":
+    df = df[df['user'] == user_filter]
+
+st.title("📜 Histori Transaksi")
+st.dataframe(df)
