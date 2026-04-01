e376c1fe29d1c8eb227c48e25981a6a4e9ac7f7f
Mufti129 <ukhtytrader01@gmail.com>
2026-04-01 07:24:12 +0700

    6_Database_Viewer.py

---
+++ pages/6_Database_Viewer.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from services.sheets_service import connect, get_data
+import config
+
+client = connect()
+
+tables = {
+    "products": config.SHEET_PRODUCTS,
+    "transactions": config.SHEET_TRANSACTIONS,
+    "sales": config.SHEET_SALES,
+    "recipe": config.SHEET_RECIPE
+}
+
+choice = st.selectbox("Pilih Data", list(tables.keys()))
+
+df = get_data(client, config.SPREADSHEET_NAME, tables[choice])
+
+st.dataframe(df)
+
+csv = df.to_csv(index=False).encode('utf-8')
+
+st.download_button(
+    "⬇️ Download CSV",
+    csv,
+    file_name=f"{choice}.csv",
+    mime="text/csv"
+)
