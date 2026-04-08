e8c0d743d411fa82299f9098753acb68cf3eb675
Mufti129 <ukhtytrader01@gmail.com>
2026-04-08 20:56:34 +0700

    2_Stok_Dashboard.py

--- pages/2_Stok_Dashboard.py
+++ pages/2_Stok_Dashboard.py
@@ -11,10 +11,10 @@ recipe = get_data(client, config.SPREADSHEET_ID, config.SHEET_RECIPE)
 
 stock = calculate_stock(trans, sales, recipe)
 
-st.title("📊 Stok Saat Ini")
+st.title("Stok Saat Ini")
 st.dataframe(stock)
 
 low = stock[stock['stok'] < 100]
 if not low.empty:
-    st.warning("⚠️ Stok menipis!")
+    st.warning("Stok menipis!")
     st.dataframe(low)
