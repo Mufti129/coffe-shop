1af4715b5a0b6351b0a930de33d37dea963b76d8
Mufti129 <ukhtytrader01@gmail.com>
2026-04-08 20:55:04 +0700

    app.py

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.set_page_config(page_title="Coffee Inventory", layout="wide")
 
-st.title("☕ Coffee Shop Inventory System")
+st.title("Coffee Shop Inventory System")
 st.markdown("Gunakan menu di sidebar")
